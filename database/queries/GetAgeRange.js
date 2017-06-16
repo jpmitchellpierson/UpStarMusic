0ed68ca43f2470c7b60f237faab606015d2e1a18
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-06-16 16:07:40 -0700

    min max and search queries

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -6,4 +6,32 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+  const minAge = Artist
+    // provides an array of all artists
+    .find({})
+    // sorts them by ascending age
+    .sort({ age: 1 })
+    // limits the array to just the first element
+    .limit(1)
+    // returns first element of limited array
+    .then(artists => artists[0].age);
+
+  const maxAge = Artist
+    .find({})
+    .sort({ age: -1 })
+    .limit(1)
+    .then(artists => artists[0].age);
+  
+  // want to return both min and max in the same object
+  return Promise.all([minAge, maxAge])
+    .then((result) => {
+      // create object to be returned
+      return { min: result[0], max: result[1]};
+    });
 };
+
+// if we wanted to use the above query
+// GetAgeRange()
+//   .then((ageObject) => {})
+//     console.log(ageObject) //{ min: 14, max: 25 }
+//   });
